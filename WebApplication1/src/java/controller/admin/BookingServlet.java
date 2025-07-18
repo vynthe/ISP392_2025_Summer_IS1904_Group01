fbab6136e5515c1d149c8b308097d0263e7d659a
NguyenTaiTuan2706 <tuannthe186498@fpt.edu.vn>
2025-07-18 10:05:24 +0700

    Create BookingServlet.java

---
+++ WebApplication1/src/java/controller/admin/BookingServlet.java
@@ -0,0 +1,35 @@
+package controller.admin;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+import model.dao.BookingDAO;
+import model.entity.Booking;
+
+@WebServlet(name = "BookingServlet", urlPatterns = {"/BookingServlet","/admin/bookings"})
+public class BookingServlet extends HttpServlet {
+
+    private BookingDAO bookingDAO;
+
+    @Override
+    public void init() throws ServletException {
+        bookingDAO = new BookingDAO();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            List<Booking> bookings = bookingDAO.getAllBookings();
+            request.setAttribute("bookings", bookings);
+            request.getRequestDispatcher("/views/user/Receptionist/bookingList.jsp").forward(request, response);
+        } catch (SQLException e) {
+            e.printStackTrace(); // hoặc dùng Logger
+            request.setAttribute("error", "Lỗi khi tải danh sách lịch hẹn.");
+            request.getRequestDispatcher("/error.jsp").forward(request, response);
+        }
+    }
+}
