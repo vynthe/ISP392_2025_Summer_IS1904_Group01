864b34e91f24f30d99b4342407be1513f73a60ea
NguyenTaiTuan2706 <tuannthe186498@fpt.edu.vn>
2025-08-03 19:06:26 +0700

    Create ReviewReply.java

---
+++ WebApplication1/src/java/model/entity/ReviewReply.java
@@ -0,0 +1,61 @@
+package model.entity;
+
+import java.sql.Timestamp;
+
+public class ReviewReply {
+    private int replyID;
+    private int reviewID;
+    private int userID;
+    private String comment;
+    private Timestamp createdAt;
+    private String userFullName;
+
+    public int getReplyID() {
+        return replyID;
+    }
+
+    public void setReplyID(int replyID) {
+        this.replyID = replyID;
+    }
+
+    public int getReviewID() {
+        return reviewID;
+    }
+
+    public void setReviewID(int reviewID) {
+        this.reviewID = reviewID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUserFullName() {
+        return userFullName;
+    }
+
+    public void setUserFullName(String userFullName) {
+        this.userFullName = userFullName;
+    }
+}
+    
